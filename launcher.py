e79f6680678305078c0d6345712d006af1712880
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-11-24 18:57:22 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
 import os
 import requests
 
-os.system("https://github.com/EletrixtimeYT/eletrixtool.git")
+os.system("git clone https://github.com/EletrixtimeYT/eletrixtool.git")
 os.system("python /eletrixtool/main.py")
 os.system("python3 /eletrixtool/main.py")
